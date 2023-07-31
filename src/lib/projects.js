76997aa7259fcccebb50a22bda7aab860b89194a
Oluwadunsin <oluwadunsin2016@gmail.com>
2023-07-31 17:05:34 +0100

    I uploaded my projects

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -1,12 +1,75 @@
 let projects = [
   {
+    id: 'Design1',
+    category: 'web',
+    title: 'printivo',
+    image: '/images/projects/printivo.png',
+    description: `It is a simple printivo front page design. Built with HTML and CSS`,
+    web_link: 'https://printivo-xi.vercel.app/',
+    github_link: 'https://github.com/Oluwadunsin2016/Printivo'
+  },
+  {
+    id: 'Design2',
+    category: 'web',
+    title: 'She Leads Africa',
+    image: '/images/projects/SLA.png',
+    description: `It is a simple SLA front page design. Built with HTML and CSS`,
+    web_link: 'https://level-one-test-bice.vercel.app/',
+    github_link: 'https://github.com/Oluwadunsin2016/LevelOne_Test'
+  },
+  {
+    id: 'Design3',
+    category: 'web',
+    title: 'Biggest University',
+    image: '/images/projects/University.png',
+    description: `A simple University web design with animation. Built with HTML, CSS and wow js`,
+    web_link: 'https://university-website-design-css.vercel.app/',
+    github_link: 'https://github.com/Oluwadunsin2016/University-Website-design-css'
+  },
+  {
+    id: 'Design4',
+    category: 'web',
+    title: 'Fitness',
+    image: '/images/projects/fitness.png',
+    description: `A simple Fitness web design with animation. Built with HTML, CSS and wow js`,
+    web_link: 'https://fitness-css.vercel.app/',
+    github_link: 'https://github.com/Oluwadunsin2016/fitness-css'
+  },
+  {
+    id: 'game',
+    category: 'web',
+    title: 'TIC TAC TOE',
+    image: '/images/projects/tic_tac_toe.png',
+    description: `A lovable tic tac toe game. Built with HTML, CSS and JavaScript`,
+    web_link: 'https://fitness-css.vercel.app/',
+    github_link: 'https://github.com/Oluwadunsin2016/fitness-css'
+  },
+  {
+    id: 'movieApp',
+    category: 'web',
+    title: 'Netflix',
+    image: '/images/projects/netflix.png',
+    description: `A clone of netlix application where you can watch the latest movie. Built with React Js and tailwind CSS`,
+    web_link: 'https://netflix-2-woad.vercel.app/',
+    github_link: 'https://github.com/Oluwadunsin2016/Netflix_2'
+  },
+  {
+    id: 'photo',
+    category: 'web',
+    title: 'Gallery',
+    image: '/images/projects/gallery.png',
+    description: `A photo gallery application that allows you to save and view your pictures so that you won't loose them. Built with React Js and tailwind CSS`,
+    web_link: 'https://gallery-gamma-three.vercel.app/',
+    github_link: 'https://github.com/Oluwadunsin2016/Netflix_2'
+  },
+  {
     id: 'ecommerce',
     category: 'web',
     title: 'ALEXISCOM',
     image: '/images/projects/ecommerce.png',
-    description: `AlexisCom provides you with the latest products with cheapest prices, shop with them and enjoy the benefits. No regrets.`,
+    description: `AlexisCom provides you with the latest products with cheapest prices, shop with them and enjoy the benefits. No regrets. Built with React Js`,
     web_link: 'https://alexiscom.netlify.app/',
-    github_link: 'https://github.com/Oluwadunsin2016/Alexiscom_client'
+    github_link: 'https://github.com/Oluwadunsin2016/Gallery'
   },
   {
     id: 'chatApp',
